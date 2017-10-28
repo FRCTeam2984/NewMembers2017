4b1170144ab05f03611b10f982de43f7490d0c06
Cameron Reikes <cameronreikes@gmail.com>
2017-10-28 11:20:51 -0700

    Added basic functions

--- src/org/usfirst/frc/team2984/robot/commands/DistanceSensor.java
+++ src/org/usfirst/frc/team2984/robot/commands/DistanceSensor.java
@@ -1,5 +1,29 @@
 package org.usfirst.frc.team2984.robot.commands;
 
-public class DistanceSensor {
+import org.usfirst.frc.team2984.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DistanceSensor extends Command {
+	
+    public DistanceSensor() {
+    	this.requires( Robot.Driving );
+    }
+    
+    protected boolean isFinished() {
+        return false;
+    }
+    
+    protected void execute() {
+    	// This is what actually runs
+    	// NON-INTERRUPTABLE
+    }
+    
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+    
+    protected void interrupted() {
+    }
 
 }
